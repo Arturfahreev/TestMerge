581f1ee80679b352a5d7c30a590fb2a3f313e1c2
ArturFahreev <jd-forever@mail.ru>
2024-09-14 15:32:41 +0300

    changes4 in feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Hello from Feature");
         System.out.println("Hello from Feature changes 2");
         System.out.println("Hello from Feature changes 3");
+        System.out.println("Hello from Feature changes 4");
     }
 }
